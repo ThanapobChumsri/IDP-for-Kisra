05bd2426a013721892ca6077efb1f9ef592517f1
Thanapob Chumsri <48666148+ThanapobChumsri@users.noreply.github.com>
2024-06-19 16:11:53 +0700

    Update FoodController.php
    add input random amount and check if random without filter

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -9,29 +9,39 @@ class FoodController extends Controller
 {
     public function filterByMaterial(Request $request)
     {
-        // Validate the request
-        $request->validate([
-            'materials' => 'required|array', // Ensure materials is required and an array
-        ]);
-
         // Get the materials array from the request
-        $materials = $request->input('materials');
+        $materials = $request->input('materials', []);
+        $requestedAmount = $request->input('amount', 1);
+        // Check if there are no materials
+        if (empty($materials)) {
+            $menu = Menu::all()->random($requestedAmount);
+            return response()->json(['menus' => $menu]);
+        } else {
+            // Query to find menus where all specified materials are included in at least one item in the `material` JSON array
+            $menus = Menu::where(function ($query) use ($materials) {
+                foreach ($materials as $material) {
+                    $query->whereRaw('JSON_CONTAINS(material, \'["' . $material . '"]\')');
+                }
+            })->get();
 
-        // Query to find menus where all specified materials are included in at least one item in the `material` JSON array
-        $menus = Menu::where(function ($query) use ($materials) {
-            foreach ($materials as $material) {
-                $query->whereRaw('JSON_CONTAINS(material, \'["' . $material . '"]\')');
+            // Filter out menus that do not contain all specified materials
+            $filteredMenus = $menus->filter(function ($menu) use ($materials) {
+                return count(array_intersect($menu->material, $materials)) === count($materials);
+            });
+
+            // Get the requested amount, defaulting to 1 if not provided
+
+            // Check if there are enough filtered menus
+            if ($filteredMenus->count() < $requestedAmount) {
+                return response()->json([
+                    'message' => 'You requested ' . $requestedAmount . ' items, but there are only ' . $filteredMenus->count() . ' items available.'
+                ], 400); // 400 Bad Request
             }
-        })->get();
 
-        // Filter out menus that do not contain all specified materials
-        $filteredMenus = $menus->filter(function ($menu) use ($materials) {
-            return count(array_intersect($menu->material, $materials)) === count($materials);
-        });
+            // Randomly select a menu
+            $randomMenu = $filteredMenus->random($requestedAmount);
 
-        // Randomly select a menu
-        $randomMenu = $filteredMenus->random();
-
-        return response()->json(['menus' => $randomMenu]);
+            return response()->json(['menus' => $randomMenu]);
+        }
     }
 }
